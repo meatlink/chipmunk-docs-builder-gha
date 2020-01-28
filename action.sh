9e1df5ce6114850ed7fb655e523609badfa8a620
Denis Potapov <denis.potapov@esr-nb-1410.esrlabs.local>
2020-01-28 09:54:05 +0100

    use rake for all entrypoint actions

--- action.sh
+++ action.sh
@@ -2,25 +2,5 @@
 set -e
 set -u
 
-
-main () {
-    generate_jekyll_site
-    generate_mdbook_content
-}
-
-generate_jekyll_site () {
-    (
-        cd "$CHIPMUNKDOCS_SRC_DIR"
-        rake build
-    )
-}
-
-generate_mdbook_content () {
-    (
-        cd "$CHIPMUNKDOCS_SRC_DIR"
-        PATH="${PATH}:/opt/mdbook" rake book
-    )
-}
-
-
-main
+rake build
+PATH="${PATH}:/opt/mdbook" rake book
